c78ee3496ebf125d2ef8fa8c11319b5892095782
Hal Seki <hal@plants-web.jp>
2016-07-27 16:10:47 +0900

    test projects validations and associations

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,5 +1,13 @@
 FactoryGirl.define do
   factory :project do
-    
+    subject "My awesome project"
+    after(:create) do |project|
+      10.times do |i|
+        create(:project_update, description:"meeting#{i}}", project: project)
+      end
+    end
+  end
+  factory :project_update do
+    description 'initial'
   end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Project, 'validation' do
+  it { should validate_presence_of :subject }
+end
+describe Project, 'association' do
+  it { should have_many(:project_updates) }
+  it { should belong_to(:user) }
+  it { should have_and_belong_to_many(:users) }
+  it { should have_and_belong_to_many(:skills) }
+  it { should belong_to(:stage) }
 end
